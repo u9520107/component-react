f8bc79e24e50f019f9e4f99a332415bfc9586be1
Jack Tzu-Chieh Huang <tcjack@meepshop.com>
2014-07-08 11:11:39 +0800

    use react for browser instead.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,32 @@
 var gulp = require('gulp');
 var fs = require('fs');
+var request = require('request');
+var path = require('path');
 
-gulp.task('copy-react', function () {
-	if(fs.existsSync(__dirname + '/node_modules/react')) {
-		gulp.src(__dirname + '/node_modules/react/lib/*.js')
-			.pipe(gulp.dest('react'));
-	
+var version = '0.10.0';
+var root = 'http://fb.me/';
+
+gulp.task('copy-react', function (cb) {
+	var url = root + 'react-' + version + '.js';
+	if(!fs.existsSync('react')) {
+		fs.mkdirSync('react');
 	}
+	var stream = request(url, function (err, resp) {
+		if(err || resp.statusCode != 200) {
+			throw new Error('Couldn\'t fetch %s: ' + (err ? err.message : 'HTTP ' + resp.statusCode));
+		}
+	});
+	var writer = fs.createWriteStream(path.resolve('react','react.js'));
+	stream.pipe(writer);
+	stream.on('error', function(err) {
+		throw err;
+	});
+	writer.on('error', function (err) {
+		throw err;
+	});
+	writer.on('end', function () {
+		cb();
+	});
 });
 
 
